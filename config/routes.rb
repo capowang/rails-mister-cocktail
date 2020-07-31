92128653d57435e6cf6d44beb9629da4935b920b
Capo Wang <capowangshuorui@gmail.com>
2020-07-31 14:01:54 +0800

    A

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     resources :doses, only: [:new, :create]
 end
 resources :doses, only: [:destroy]
-root to: 'pages#home'
+root to: 'cocktails#index'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
